5e3a31bf3b763b71ab1c8ea5b16d9c18cf1f2de2
Michelle <mtlassig@gmail.com>
2016-01-13 14:14:21 -0600

    Just starting. Added funtionality to increment the size as we push into it.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,19 @@ var Stack = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+  var counter = 0;
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    counter++;
   };
 
   someInstance.pop = function(){
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
