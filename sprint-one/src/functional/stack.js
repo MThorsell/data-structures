59b2c6b5123c530ab755f80227c2fe7e55ba7570
Michelle <mtlassig@gmail.com>
2016-01-14 13:14:26 -0600

    Completed Functional for Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -7,10 +7,17 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[counter] = value;
     counter++;
   };
 
   someInstance.pop = function(){
+    if (counter > 0){
+      counter--;
+      var temp = storage[counter];
+      delete storage[counter];
+      return temp;
+    };
   };
 
   someInstance.size = function(){
