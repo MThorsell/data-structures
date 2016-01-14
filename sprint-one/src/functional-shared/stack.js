11bbe6fcd559023e62de0028603a32a9389ebb4a
Michelle <mtlassig@gmail.com>
2016-01-14 14:12:55 -0600

    Most tests passing. Need final test to pass

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {
+    // counter: 0
+  };
+  storage.counter = 0;
+  storage.size = stackMethods.size;
+  storage.push = stackMethods.push;
+  storage.pop = stackMethods.pop;
+
+  return storage;
+
 };
 
 var stackMethods = {};
+
+stackMethods.size = function(){
+    return this.counter;
+};
+
+stackMethods.push = function(value) {
+  this[this.counter] = value;
+  this.counter++;
+};
+
+stackMethods.pop = function() {
+  if (this.counter > 0) {
+    var temp = this[this.counter];
+    delete this[this.counter];
+    this.counter--;
+    return temp;
+  }
+ 
+};
 
 
