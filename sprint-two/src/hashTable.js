22bb44f3b901b69157b7d9bb1fec6b02176dd930
Michelle <mtlassig@gmail.com>
2016-01-16 12:14:52 -0600

    Completed basic requirements of Hash Table. Yay!

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,46 +5,78 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  //Check if there is already an array for that bucket
-  if (this._storage.set(i) === undefined) {
+  // Get the bucketArray out of the specified storage index.
+  var bucketArray = this._storage.get(i);
+  // If there's nothing at the storage index, create the bucketArray
+  // and add its first k/v pair.
+  if (bucketArray === undefined) {
     this._storage.set(i, [[k, v]]);
+    console.log(this._storage.get(i));
   } else {
-    //if so, use variable added to keep track of if we've added the key-value pair yet
-    var added = false;
-    //Iterate through the arrays that are in the bucket array
-    for (var n = 0; n < this._storage.set(i); n++) {
-      //Check if any of them have the key k in them
-      if (this._storage.set(i)[n][0] === k) {
-        //if they do, overwrite the value of that array with v
-        this._storage.set(i)[n][1] = v;
-        //and update added to say that we've added our key-value pair
-        added = true;
+    var keyExists = false;
+    // Iterate through the bucketArray to check for key matches
+    for (var n = 0; n < bucketArray.length; n++) {
+      var storedKey = bucketArray[n][0];
+      var storedValue = bucketArray[n][1];
+      // If the key already exists in bucketArray, overwrite the
+      // key's value.
+      if (storedKey === k) {
+        bucketArray[n][1] = v;
+        console.log(storedValue);
+        keyExists = true;
+        // Send updated bucketArray back to storage.
+        this._storage.set(i, bucketArray);
       }
     }
-    //if there is already a bucket array, but it doesn't include an array with key k, just add that key-value array to the curent bucket
-    if (added = false) {
-      this._storage.set(i, [k, v]);
+    // If the key does not already exist in the bucketArray, push
+    // its k/v pair to the bucketArray.
+    if (keyExists === false) {
+      bucketArray.push([k, v]);
+      // Send updated bucketArray back to storage.
+      this._storage.set(i, bucketArray);
+      keyExists = false;
     }
   }
-  
-  
 };
-//Need to update retrieve to search through each bucket array to find the array with the correct key
+
+
 HashTable.prototype.retrieve = function(k){
+  //get your index
   var i = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage.get(i) === null){
-    return null;
+  //if there is alreday a bucket, iteratate through the bucket to check each array
+  for (var n = 0; n < this._storage.get(i).length; n++){
+    //if the current array has k in position 1, 
+    if (this._storage.get(i)[n][0] === k){
+      //return the value of that array
+      return this._storage.get(i)[n][1];
+    }
   }
-  return (this._storage.get(i))[1];
+  //return null if we have not found an array in that bucket with the requested k value
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, null);
+  // Get the bucketArray out of the specified storage index.
+  var bucketArray = this._storage.get(i);
+  // Iterate through the bucketArray.
+  // When k is found, remove the k/v pair.
+  for (var storedKVPair = 0; storedKVPair < bucketArray.length; storedKVPair++) {
+    var aStoredKey = bucketArray[storedKVPair][0];
+    var aStoredValue = bucketArray[storedKVPair][1];    
+    if (aStoredKey === k) {
+      bucketArray.splice(storedKVPair, 1);
+    }
+  }
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert is constant time 
+ retrieve is constant time
+ remove is constant time
  */
+ 
+
